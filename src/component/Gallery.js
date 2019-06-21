9d56ae94f6ff2b0ba9ab9cf64c0fea989d767027
ezra davis davis <ezra.rr.davis@gmail.com>
2019-06-21 14:53:37 -0700

    created loading component

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -2,24 +2,12 @@ import React, { useState, useEffect } from 'react';
 import ArtCard from './ArtCard.js';
 import axios from 'axios';
 
-const Gallery = () => {
-  const [imgList, setImgList] = useState([]);
+const Gallery = ({ imgList }) => {
   const [displayLarge, setDisplayLarge] = useState(false);
+
   const changeDisplay = () => {
     setDisplayLarge(!displayLarge);
   };
-  useEffect(() => {
-    axios
-      .get('https://www.instagram.com/langston_alimayu/?__a=1')
-      .then(({ data }) => {
-        const posts = data.graphql.user.edge_owner_to_timeline_media.edges;
-        const img = [];
-        for (let i = 0; i < posts.length; i++) {
-          img.push(posts[i].node.display_url);
-        }
-        setImgList(img);
-      });
-  }, []);
   return (
     <>
       <i
--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Gallery from './Gallery';
 import Footer from './Footer';
 
 const LandingPage = () => {
+  const [imgList, setImgList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get('https://www.instagram.com/langston_alimayu/?__a=1')
+      .then(({ data }) => {
+        const posts = data.graphql.user.edge_owner_to_timeline_media.edges;
+        const img = [];
+        for (let i = 0; i < posts.length; i++) {
+          img.push(posts[i].node.display_url);
+        }
+        setImgList(img);
+        setLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, []);
   return (
-    <div className='landing'>
-      <Gallery />
-      <Footer />
-    </div>
+    <>
+      {!loading ? (
+        <div className='landing'>
+          <Gallery imgList={imgList} />
+          <Footer />
+        </div>
+      ) : (
+        <div className='loading'>
+          <i class='fas fa-circle-notch fa-spin fa-7x' />
+        </div>
+      )}
+    </>
   );
 };
 
